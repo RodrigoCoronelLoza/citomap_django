1bc600ff3afad585ab5d4fd206574716950c9d9c
RodrigoCoronelLoza <drigo31416@gmail.com>
2024-06-26 13:40:14 -0400

    clean code urls

--- hospital/urls.py
+++ hospital/urls.py
@@ -1,4 +1,3 @@
-# from django.contrib import admin
 from django.urls import path
 from .views import About,Home,Contact,Index,Login,Logout_admin,View_Doctor,Delete_Doctor,Add_Doctor,Ver_Informe,View_Informe,Report
 from .views import Add_Informe_Cit,Upd_Datos_Paciente,Upd_Muestra_Paciente,Upd_Tabla_Central_Paciente, Upd_Conclusion_Paciente
@@ -14,19 +13,9 @@ urlpatterns = [
     path('logout/', Logout_admin, name='logout_admin'),
     path('index/', Index, name='dashboard'),
     path('view_doctor/', View_Doctor, name='view_doctor'),
-    path('add_doctor/', Add_Doctor, name='add_doctor'),
-    
-    # path('view_patient/', View_Patient, name='view_patient'),
-    # path('view_appointment/', View_Appointment, name='view_appointment'),
-    # path('add_patient/', Add_Patient, name='add_patient'),
-    # path('add_appointment/', Add_Appointment, name='add_appointment'),
-    path('delete_doctor(?p<int:pid>)/', Delete_Doctor, name='delete_doctor'),
-    # path('delete_patient(?p<int:pid>)/', Delete_Patient, name='delete_patient'),
-    # path('delete_appointment(?p<int:pid>)/', Delete_Appointment, name='delete_appointment'),
-
-    # path('add_informe/',Add_Informe,name='add_informe'),
-    path('view_informe/',View_Informe,name='view_informe'),
-    # path('delete_informe(?p<int:pid>)/', Delete_Informe, name='delete_informe'),
+    path('add_doctor/', Add_Doctor, name='add_doctor'),  
+    path('delete_doctor(?p<int:pid>)/', Delete_Doctor, name='delete_doctor'),      
+    path('view_informe/',View_Informe,name='view_informe'), 
     path('ver_informe(?p<int:pid>)/',Ver_Informe,name='ver_informe'),
     
     path('report(?p<int:pid>)/',Report,name='report'),
